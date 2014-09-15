bd277887abc79875d689aee9c8117b29c6f5ef2e
James Watterson <jameslwatterson@gmail.com>
2014-09-15 12:29:43 -0400

    argument check

--- lab1/p1/reminder.c
+++ lab1/p1/reminder.c
@@ -1,5 +1,25 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+
+#define MAX_REMINDERS 100
+#define MAX_REMINDER_LEN 140 //length of reminder string
+
+//reminder array
+char reminders[MAX_REMINDERS][MAX_REMINDER_LEN];
+
+void read_reminders(char *filename);
 
 int main(int argc, char **argv){
-	printf("YOLO\n");
+	if(argc < 2){
+		printf("Syntax Error\n");
+		printf("usage:\n\tmyreminder filename\n");
+		exit(-1);
+	}
+	read_reminders(argv[1]);
+}
+
+void read_reminders(char *filename){
+
 }
